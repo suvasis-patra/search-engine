d6ee5b354e2a3e54d896d61c4d8e208170736fee
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2026-07-10 12:00:49 +0530

    add request logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
 
+import { reqLogger } from "./middleware/reqLoggger.middleware";
 import { errorMiddleware } from "./middleware/error.middleware";
 import { healtCheckController } from "./controller/healt.controller";
 
 const app = express();
 
+app.use(reqLogger);
 app.use(express.json());
 app.use(cors({ origin: ["http://localhost:3000"] }));
 
